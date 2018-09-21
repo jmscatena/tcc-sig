baafd1ca3e56f0d219809b54a131e99de54a7172
jmscatena <jean.scatena@fatec.sp.gov.br>
2018-09-21 17:55:07 -0300

    Adição de Recursos de CSS e do Formulário de Cadastro de Professores

---
+++ SIGTCC/src/java/bean/BeanProfessor.java
@@ -0,0 +1,21 @@
+
+package bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author Prof.Jean Miler Scatena
+ * @see Bean Gerenciado para Cadastro de Professores
+ * @version 0.1
+ * @date 09-21-2018
+ */
+@ManagedBean
+@RequestScoped
+public class BeanProfessor {
+
+    public BeanProfessor() {
+    }
+    
+}
